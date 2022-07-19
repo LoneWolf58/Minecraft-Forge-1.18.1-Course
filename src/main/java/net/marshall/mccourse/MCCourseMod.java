8c7e0ea192f352ebe2c54a89b8114d4f637a13c4
lonewolf58 <africawolf58@gmail.com>
2022-07-19 19:35:09 +0100

    Assignment 5: Custom Creative Items & Blocks

--- src/main/java/net/marshall/mccourse/MCCourseMod.java
+++ src/main/java/net/marshall/mccourse/MCCourseMod.java
@@ -1,5 +1,6 @@
 package net.marshall.mccourse;
 
+import net.marshall.mccourse.block.ModBlocks;
 import net.marshall.mccourse.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -34,6 +35,7 @@ public class MCCourseMod
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
--- src/main/java/net/marshall/mccourse/item/ModItems.java
+++ src/main/java/net/marshall/mccourse/item/ModItems.java
@@ -6,7 +6,6 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.IRegistryDelegate;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
@@ -19,7 +18,10 @@ public class ModItems {
     public static RegistryObject<Item> COBALT_NUGGET = ITEMS.register("cobalt_nugget",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
-    public  static void register(IEventBus eventBus){
+    public static RegistryObject<Item> RAW_COBALT = ITEMS.register("raw_cobalt",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
 }
